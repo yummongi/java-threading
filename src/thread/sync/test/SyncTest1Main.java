3929c7ee0eee17ef6dc45f5f4dcff82663ebf39a
yummongi <yummongi@gmail.com>
2024-08-25 14:16:58 +0900

    [섹션7] 동기화 - synchronized: 학습 완료
    - synchronized 메서드와 블록의 차이점 학습
    - 출금 예제를 통한 동시성 문제 실습
    - 임계 영역 개념 이해 및 적용

---
+++ src/thread/sync/test/SyncTest1Main.java
@@ -0,0 +1,40 @@
+package thread.sync.test;
+
+public class SyncTest1Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    counter.increment();
+                }
+            }
+        };
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+
+        thread1.start(); // 10000
+        thread2.start(); // 10000
+
+        thread1.join();
+        thread2.join();
+        System.out.println("결과: " + counter.getCount());
+
+    }
+
+    static class Counter {
+        private int count = 0;
+
+        public synchronized void increment() {
+            count += 1;
+        }
+
+        public synchronized int getCount() {
+            return count;
+        }
+    }
+}
