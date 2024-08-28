012243caae3b16f6795865decf2433cb550fafce
yummongi <yummongi@gmail.com>
2024-08-28 13:23:17 +0900

    [섹션8] 고급 동기화 - concurrent.Lock: 학습 완료
    - LockSupport 클래스의 기능과 사용법 학습
    - ReentrantLock의 이론과 실제 활용 방법 이해
    - Lock 인터페이스와 Condition 객체 활용 방법 습득
    - 대기 중단 기능을 포함한 고급 동기화 기법 학습

---
+++ src/thread/sync/lock/LockSupportMainV2.java
@@ -0,0 +1,28 @@
+package thread.sync.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportMainV2 {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ParkTest(), "Thread-1");
+        thread1.start();
+
+        // 잠시 대기하여 Thread-1이 park 상태에 빠질 시간을 준다.
+        sleep(100);
+        log("Thread-1 state: " + thread1.getState()); // park 일때 상태
+    }
+
+    static class ParkTest implements Runnable {
+
+        @Override
+        public void run() {
+            log("park 시작");
+            LockSupport.parkNanos(2000_000000); // parkNanos 사용 : 2초 뒤 깨어남
+            log("park 종료, state : " + Thread.currentThread().getState());
+            log("인터럽트 상태 : " + Thread.currentThread().isInterrupted());
+        }
+    }
+}
